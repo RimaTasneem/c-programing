3ec5739ae30ba6f82a29588d3ead14211b2ff886
RimaTasneem <rimatasneem2004@gmail.com>
2025-03-10 16:30:14 +0530

    Add files via upload

---
+++ fibbo by recursion.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+# include<math.h>
+int fibonacci(int n)
+{
+	if (n==0)
+	return 0;
+	else if (n==1)
+	return 1;
+	else
+	return fibonacci (n-1)+fibonacci (n-2);
+}
+int main()
+{
+	int i,n;
+	printf("Enter number of term:"),
+	scanf ("%d",&n);
+	for (i=0;i<n;i++)
+	printf("%d",fibonacci(i));
+	return 0;
+}
